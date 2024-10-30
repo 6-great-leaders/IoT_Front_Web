5a3f26f096f6f22837a2f20e6b38c7756d55e186
vlenel <victor.lenel@epita.fr>
2024-10-30 11:59:40 +0100

    removed quiet flag

--- monitor/eslint.config.js
+++ monitor/eslint.config.js
@@ -5,18 +5,19 @@ import pluginVue from "eslint-plugin-vue";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  {files: ["**/*.vue"],
-    languageOptions: {parserOptions: {parser: tseslint.parser} },
-    rules: {
+    { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...pluginVue.configs["flat/essential"],
+    {
+      files: ["**/*.vue"],
+      languageOptions: { parserOptions: { parser: tseslint.parser } },
+      rules: {
         "@typescript-eslint/no-explicit-any": "warn",
         "@typescript-eslint/no-empty-object-type": "warn",
         "vue/multi-word-component-names": "warn",
         "@typescript-eslint/triple-slash-reference": "warn"
       }
-},
-];
+    }
+  ];  
